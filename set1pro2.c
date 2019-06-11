73ab3318667c0700a81397363a0c26e585b3708d
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-06-11 09:32:32 +0530

    Create set1pro2.c

---
+++ set1pro2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int num;
+scanf("%d",&num);
+if(num<=0)
+printf("invalid");
+else if(num%2==0)
+printf("Even");
+else if(num%2==1)
+printf("Odd");
+	
+	
+	
+	
+	return 0;
+}
